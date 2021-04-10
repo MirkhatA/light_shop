06f47faab768240499ad6df101e8e486049b9caa
Mirkhat <MirkhatA@users.noreply.github.com>
2021-04-11 02:46:12 +0600

    Initial commit

---
+++ footer.php
@@ -0,0 +1,15 @@
+    <!-- the cdn of glidejs -->
+    <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+
+    <!-- Simply reference a minified version of the script. -->
+    <script>
+        new Glide('.glide').mount()
+    </script>
+
+    <!-- script.js connection -->
+    <script src="script/script.js"></script>
+    
+    <!-- bootstrap connection -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,73 @@
+<!-- header connection -->
+<?php require_once 'header.php'; ?>
+
+<!-- carousel -->
+<div class="slider">
+    <input class="slider__nav" type="radio" name="slider" title="slide1" checked="checked"/>
+    <input class="slider__nav" type="radio" name="slider" title="slide2"/>
+    <input class="slider__nav" type="radio" name="slider" title="slide3"/>
+    <input class="slider__nav" type="radio" name="slider" title="slide4"/>
+
+    <div class="slider__inner">
+        <div class="slider__contents slide1">
+            <i class="slider__image fa fa-codepen"></i>
+            <h2 class="slider__caption">codepen</h2>
+            <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+        </div>
+
+        <div class="slider__contents slide2">
+            <i class="slider__image fa fa-newspaper-o"></i>
+            <h2 class="slider__caption">newspaper-o</h2>
+            <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+        </div>
+
+        <div class="slider__contents slide3">
+            <i class="slider__image fa fa-television"></i>
+            <h2 class="slider__caption">television</h2>
+            <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+        </div>
+
+        <div class="slider__contents slide4">
+            <i class="slider__image fa fa-diamond"></i>
+            <h2 class="slider__caption">diamond</h2>
+            <p class="slider__txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cupiditate omnis possimus illo quos, corporis minima!</p>
+        </div>
+    </div>
+</div>
+<!-- carousel -->
+
+<!-- category -->
+<section id="category">
+    <div class="container-fluid">
+        <div class="text-center">
+            <h2 class="category__headline">Категории</h2>  
+        </div>
+        
+        <!-- category-carousel -->
+        <div class="container">
+            <div class="glide">
+                <div class="glide__track" data-glide-el="track">
+                    <ul class="glide__slides">
+                        <li class="glide__slide">0</li>
+                        <li class="glide__slide">1</li>
+                        <li class="glide__slide">2</li>
+                    </ul>
+                </div>
+                <div class="glide__arrows" data-glide-el="controls">
+                    <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+                    <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- button -->
+        <button class="category__button">
+            Подробнее
+        </button>
+    </div>
+</section>
+<!-- category -->
+
+
+<!-- footer connection -->
+<?php require_once 'footer.php'; ?>
